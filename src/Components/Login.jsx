1e29c7d096e731581a511c30e0e060dcfe8225f8
Zeeshan Afzal Khan <zeeshasn2893@gmail.com>
2025-08-29 23:39:43 +0530

    Login/SignUp/Slash pages UI

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,76 @@
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-        <div className='w-full h-[150vh] bg-[url("https://assets.nflxext.com/ffe/siteui/vlv3/cb72daa5-bd8d-408b-b949-1eaef000c377/web/IN-en-20250825-TRIFECTA-perspective_a3209894-0b01-4ddb-b57e-f32165e20a3f_large.jpg")] bg-cover'>
-            <h1 className="text-white">Login</h1>
-        </div>
-    </div>
-  )
-}
+  const [isSignIn, setIsSignIn] = useState(true);
 
-export default Login
+  return (
+    <div className='w-full h-[150vh] flex justify-center bg-[url("https://assets.nflxext.com/ffe/siteui/vlv3/cb72daa5-bd8d-408b-b949-1eaef000c377/web/IN-en-20250825-TRIFECTA-perspective_a3209894-0b01-4ddb-b57e-f32165e20a3f_large.jpg")] bg-cover'>
+      <form className="w-4/12 h-6/12 bg-black/85 flex flex-col p-8 mt-28 mx-auto rounded-sm">
+        <div className="text-white text-3xl font-bold mb-6">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </div>
+        <div className="relative w-full">
+          <input
+            type="email"
+            placeholder="Email or Mobile number"
+            className="peer w-full p-4 text-white bg-transparent rounded-sm ring-1 ring-gray-50 focus:ring-2 focus:ring-gray-200 placeholder-transparent focus:outline-none mb-6"
+          />
+          <label
+            htmlFor="email"
+            className="absolute pointer-events-none left-4 top-4 text-gray-400 transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-focus:top-1 peer-focus:text-sm peer-focus:text-gray-400"
+          >
+            Email or Mobile number
+          </label>
+        </div>
+
+        {
+          !isSignIn && (
+            <div className="relative w-full">
+              <input
+                type="text"
+                placeholder="Full Name"
+                className="peer w-full p-4 text-white bg-transparent rounded-sm ring-1 ring-gray-50 focus:ring-2 focus:ring-gray-200 placeholder-transparent focus:outline-none mb-6" />
+              <label
+                htmlFor="fullname"
+                className="absolute pointer-events-none left-4 top-4 text-gray-400 transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-focus:top-1 peer-focus:text-sm peer-focus:text-gray-400"
+              >
+                Full Name
+              </label>
+            </div>
+          )
+        }
+
+        <div className="relative w-full">
+          <input
+            type="password"
+            placeholder="Password"
+            className="peer w-full p-4 text-white bg-transparent rounded-sm ring-1 ring-gray-50 focus:ring-2 focus:ring-gray-200 placeholder-transparent focus:outline-none mb-6"
+          />
+          <label
+            htmlFor="password"
+            className="absolute pointer-events-none left-4 top-4 text-gray-400 transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-focus:top-1 peer-focus:text-sm peer-focus:text-gray-400"
+          >
+            Password
+          </label>
+        </div>
+        {
+
+        }
+        <button className="bg-red-600 p-2 text-white rounded-sm font-semibold text-xl">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="text-gray-400 mt-6">
+          {isSignIn ? "New to Netflix?" : "Already have an account?"}{" "}
+          <span
+            className="text-white hover:underline cursor-pointer"
+            onClick={() => setIsSignIn(!isSignIn)}
+          >
+            {isSignIn ? "Sign up now." : "Sign in now."}
+          </span>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- src/Components/Slash.jsx
+++ src/Components/Slash.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Slash = () => {
   return (
-    <div>Slash</div>
-  )
-}
+    <div className='w-full h-[150vh] bg-[url("https://assets.nflxext.com/ffe/siteui/vlv3/cb72daa5-bd8d-408b-b949-1eaef000c377/web/IN-en-20250825-TRIFECTA-perspective_a3209894-0b01-4ddb-b57e-f32165e20a3f_large.jpg")] bg-cover flex justify-center items-center '>
+      <div className="w-full h-full bg-black/70 flex justify-center mt-40">
+        <div className="w-6/12 h-6/12  flex flex-col justify-center items-center text-center px-4">
+          <div className="text-white text-5xl font-extrabold mb-6">
+            Unlimited movies, TV shows and more.
+          </div>
+          <div className="text-white text-2xl mb-6">
+            Watch anywhere. Cancel anytime.
+          </div>
+          <div className="text-white text-xl mb-6">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </div>
+          <div className="flex items-center">
+            <div className="relative">
+              <input
+                type="text"
+                id="email"
+                placeholder=" "
+                className="peer p-4 w-80 text-white bg-transparent rounded-sm ring-1 ring-gray-50 
+                 focus:ring-2 focus:ring-gray-200 focus:outline-none placeholder-transparent"
+              />
+              <label
+                htmlFor="email"
+                className="absolute pointer-events-none left-4 top-4 text-gray-400 transition-all
+                 peer-placeholder-shown:top-4 peer-placeholder-shown:text-base
+                 peer-focus:top-1 peer-focus:text-sm peer-focus:text-gray-400"
+              >
+                Email address
+              </label>
+            </div>
 
-export default Slash
+            <button className="bg-red-600 text-white px-6 py-3 text-xl font-semibold rounded-sm ml-2">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slash;
